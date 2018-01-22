4c6cfbf850f569891adb8bed870dd3cb89b2497b
sunbin <>
2018-01-22 15:53:50 +0800

    first commit

---
+++ SpaceGame/FirstScene.swift
@@ -0,0 +1,13 @@
+//
+//  FirstScene.swift
+//  SpaceGame
+//
+//  Created by 韩小胜 on 2018/1/22.
+//  Copyright © 2018年 sun. All rights reserved.
+//
+
+import UIKit
+
+class FirstScene: SKScene {
+
+}
